6f8880618a5917a5e95ff81df73a0d47d440c88a
rushabhjswl <rushabhjswl@gmail.com>
2019-08-07 00:44:06 -0700

    admin page UI

---
+++ app/src/main/java/com/example/quizzy/AdminPage.java
@@ -0,0 +1,20 @@
+package com.example.quizzy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class AdminPage extends AppCompatActivity {
+
+   // EditText question,option1,option2,option3,option4,correct_answer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_page);
+
+    }
+
+
+}
