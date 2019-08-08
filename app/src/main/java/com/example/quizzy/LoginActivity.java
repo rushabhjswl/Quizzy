f2d69117da73cc77c2aa188ae830b507ec4df5ee
aslamamaan <aslamamaan3@gmail.com>
2019-08-07 21:26:50 -0700

    Added validation on login activity.

--- app/src/main/java/com/example/quizzy/LoginActivity.java
+++ app/src/main/java/com/example/quizzy/LoginActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -48,51 +49,77 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                edt = findViewById(R.id.login_username);
-                username = edt.getText().toString();
+                setAllStrings();
 
-                edt = findViewById(R.id.login_password);
-                password = edt.getText().toString();
+                if(areEntriesValid() == true){
+                    reference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                reference.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                        if(dataSnapshot.child(username).exists()){
+                            if(dataSnapshot.child(username).exists()){
 
 
-                            User user = dataSnapshot.child(username).getValue(User.class);
-                            String usertype = user.username;
+                                User user = dataSnapshot.child(username).getValue(User.class);
+                                String usertype = user.username;
 
 
-                            if(usertype.equals("admin") && username.equals(user.username) && password.equals(user.password)){
-                                Log.i("In ADMIN Login", "Success");
-                                Intent intent = new Intent(LoginActivity.this, AdminPage.class);
-                                intent.putExtra(EXTRA_MESSAGE, username);
-                                startActivity(intent);
+                                if(usertype.equals("admin") && username.equals(user.username) && password.equals(user.password)){
+                                    Log.i("In ADMIN Login", "Success");
+                                    Intent intent = new Intent(LoginActivity.this, AdminPage.class);
+                                    intent.putExtra(EXTRA_MESSAGE, username);
+                                    startActivity(intent);
+                                }
+                                //we start the UserProfile Activity
+                                else{
+                                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+
+
+                                }
+
+
+
                             }
-                            //we start the UserProfile Activity
                             else{
-
-
-
+                                Toast.makeText(getApplicationContext(), "User does not exist", Toast.LENGTH_SHORT).show();
                             }
-
-
 
                         }
 
-                    }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        }
+                    });
+                }
 
-                    }
-                });
+
 
 
             }
         });
 
     }
+
+    public void setAllStrings(){
+
+        edt = findViewById(R.id.login_username);
+        username = edt.getText().toString();
+
+        edt = findViewById(R.id.login_password);
+        password = edt.getText().toString();
+
+
+    }
+
+    public boolean areEntriesValid() {
+
+        //if any field is empty, return false
+        if (username.equals("") || password.equals("")) {
+            Toast.makeText(getApplicationContext(), "Please Fill all Fields", Toast.LENGTH_SHORT).show();
+            return false;
+        }else
+            return true;
+    }
+
+
 }
