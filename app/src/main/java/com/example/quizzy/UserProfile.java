497ca64f9e2382b88e4fd7ff1a5b2a5e4e664915
aslamamaan <aslamamaan3@gmail.com>
2019-08-11 22:35:48 -0700

    Graph addded on User Profile

--- app/src/main/java/com/example/quizzy/UserProfile.java
+++ app/src/main/java/com/example/quizzy/UserProfile.java
@@ -3,16 +3,33 @@ package com.example.quizzy;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -22,11 +39,18 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserProfile extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private TextView txtView;
     String username;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference = database.getReference().child("Scores");
+    DatabaseReference reference2;
+    long scores[] = new long[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +63,11 @@ public class UserProfile extends AppCompatActivity
         username = intent.getStringExtra("username");
         txtView = findViewById(R.id.usergreeting);
         txtView.setText("Hello " + username);
+
+
+        //drawing chart of last 5 quiz scores
+        drawChartOfPreviousScores();
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -114,4 +143,69 @@ public class UserProfile extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void drawChartOfPreviousScores(){
+        final AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        //anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+
+        reference.child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (int i = 1; i <=5; i++) {
+                    String score = dataSnapshot.child(i + "").getValue(String.class);
+
+                    Log.e("Hello", "score " + score);
+                    scores[i - 1] = Long.parseLong(score);
+                    Log.e("array values" ,scores[i-1]+"");
+                }
+
+                Cartesian cartesian = AnyChart.column();
+                List<DataEntry> data = new ArrayList<>();
+                int countOfScores = 0;
+                Log.e("array 3" ,scores[3]+"");
+                for (int i = scores.length; i > 0; i--) {
+                    data.add(new ValueDataEntry("Quiz " + i, scores[i - 1]));
+
+                    countOfScores++;
+                    if (countOfScores == 5) {
+                        break;
+                    }
+
+                }
+                Column column = cartesian.column(data);
+
+                column.tooltip()
+                        .titleFormat("{%X}")
+                        .position(Position.CENTER_BOTTOM)
+                        .anchor(Anchor.CENTER_BOTTOM)
+                        .offsetX(0d)
+                        .offsetY(5d)
+                        .format("${%Value}{groupsSeparator: }");
+
+                cartesian.animation(true);
+                cartesian.title("Your Scores");
+
+                cartesian.yScale().minimum(0d);
+
+                cartesian.yAxis(0).labels().format("${%Value}{groupsSeparator: }");
+
+                cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+                cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+                //cartesian.xAxis(0).title("Product");
+                //cartesian.yAxis(0).title("Revenue");
+
+                anyChartView.setChart(cartesian);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
 }
